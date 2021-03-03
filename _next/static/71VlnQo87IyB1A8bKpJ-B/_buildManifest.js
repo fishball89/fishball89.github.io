6c90e48221d206c21af9e847c1221fbc02f4416a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-03 12:57:35 +0000

    Deploy fishball89/fishball89.github.io to github.com/fishball89/fishball89.github.io.git:master

---
+++ _next/static/71VlnQo87IyB1A8bKpJ-B/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9fd2a92907d9e764c1c2.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
